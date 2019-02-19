f30f3b60eb9bb05aa17a997617090a5bf422b93b
Master <git@github.com:emmolka/NavBar.git>
2019-02-19 10:13:25 +0100

    keep-on-track

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 function addMenu(){
     let a = document.createElement('ul');
-    a.classList.add('ulMenu');
-    document.querySelector('body').appendChild(a);
-    let x = document.createElement('li');
-    x.innerHTML='haha';
-    let y = document.createElement('li');
-    y.innerHTML='hehe';
-    let z = document.createElement('li');
-    z.innerHTML='hihi';
-    document.querySelector('.ulMenu').appendChild(x);
-    document.querySelector('.ulMenu').appendChild(y);
-    document.querySelector('.ulMenu').appendChild(z);
+    a.classList.add('ul-menu');
+    document.querySelector('.rolled-menu').appendChild(a);
+    let b = document.createElement('li');
+    b.innerHTML='Strona glowna';
+    let c = document.createElement('li');
+    c.innerHTML='O nas';
+    let d = document.createElement('li');
+    d.innerHTML='Oferta';
+    let e = document.createElement('li');
+    e.innerHTML='Samochody';
+    let f = document.createElement('li');
+    f.innerHTML='Kontakt';
+    a.animate({
+        opacity: [ 0, 0.9, 1 ],       
+        offset: [ 0, 0.8 ],
+        
+        
+         
+      }, 2000);
+    document.querySelector('.ul-menu').appendChild(b);
+    document.querySelector('.ul-menu').appendChild(c);
+    document.querySelector('.ul-menu').appendChild(d);
+    document.querySelector('.ul-menu').appendChild(e);
+    document.querySelector('.ul-menu').appendChild(f);
 }
 function removeMenu(){
-    let a = document.querySelector('.ulMenu');
+    let a = document.querySelector('.ul-menu');
     a.remove('ul');
     
 }
 document.querySelector('.menu').addEventListener('click', function(){
-    let check = document.querySelector('.ulMenu');
+    let check = document.querySelector('.ul-menu');
     if(check){
         removeMenu();
     }else{
